42b9c5bbb2ed49134d6779faf395d6dbc752527f
Tate Brasel <tatebrasel@gmail.com>
2021-12-10 12:25:08 -0800

    feat: format NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,16 +3,18 @@ import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
-    <div>
-      <nav className={`navbar navbar-expand-sm navbar-light px-3`}>
-        <a className="navbar-brand" href="https://tabrasel.github.io/">Tate Brasel</a>
+    <div className={styles.NavBar}>
+      <nav className={`navbar navbar-expand-sm navbar-light container-sm px-3`}>
+        <a className={styles.navBarBrand} href="https://tabrasel.github.io/">Tate Brasel</a>
+
         <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
+              <a className={`${styles.navLink} nav-link`} href="#">Projects</a>
             </li>
           </ul>
         </div>
