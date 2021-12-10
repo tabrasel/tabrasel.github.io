8a121d3083490e24e8bb27a5087b4bc202e4a085
Tate Brasel <tatebrasel@gmail.com>
2021-12-09 20:52:03 -0800

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.module.css';
 
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 import About from './About';
 import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <NavBar />
+    <HashRouter>
+      <div className={styles.App}>
+        <NavBar />
 
-      <div className="container p-4">
-        <About />
-      </div>
+        <div className="container-sm p-4">
+          <Routes>
+            <Route exact path="/" element={<About />} />
+          </Routes>
+        </div>
 
-      <div className="p-3">
-        <hr />
-        <div className="d-flex justify-content-around">
+        <footer className="p-4">
           <div className={styles.linksList}>
             <a href="mailto:tatebrasel@gmail.com"><i className="fas fa-envelope"></i> Email</a>
             <a href="https://www.linkedin.com/in/tate-brasel/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> Linkedin</a>
             <a href="https://github.com/tabrasel" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> GitHub</i></a>
           </div>
-        </div>
+        </footer>
       </div>
-
-    </div>
+    </HashRouter>
   );
 }
 
 export default App;
+
+
+
+/*
+<div className={styles.App}>
+  <NavBar />
+
+  <div className="container p-4">
+    <About />
+  </div>
+
+  <div className="p-3">
+    <hr />
+    <div className="d-flex justify-content-around">
+      <div className={styles.linksList}>
+        <a href="mailto:tatebrasel@gmail.com"><i className="fas fa-envelope"></i> Email</a>
+        <a href="https://www.linkedin.com/in/tate-brasel/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> Linkedin</a>
+        <a href="https://github.com/tabrasel" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> GitHub</i></a>
+      </div>
+    </div>
+  </div>
+</div>
+*/
