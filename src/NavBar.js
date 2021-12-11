8927327f26201765bf6921db4e1ca87be61bf039
Tate Brasel <tatebrasel@gmail.com>
2021-12-10 16:16:07 -0800

    refactor: use router instead of anchor to homepage

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <div className={styles.NavBar}>
       <nav className={`navbar navbar-expand-sm navbar-light container-sm px-3`}>
-        <a className={styles.navBarBrand} href="https://tabrasel.github.io/">Tate Brasel</a>
+        <Link className={styles.navBarBrand} to="/">Tate Brasel</Link>
 
         <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
